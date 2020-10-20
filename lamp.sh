522b343dca04db72e0b1628f1e26516cb324cc06
Natan Cabral <natancabral@gnial.com.br>
2020-10-20 18:15:33 -0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -95,6 +95,7 @@ sudo service apache2 restart
 
 
 #----------------------------------------------------------
-#https://github.com/aamnah/bash-scripts/blob/master/install/amp_debian.sh
+# https://github.com/aamnah/bash-scripts/blob/master/install/amp_debian.sh
+# https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04
 
 
