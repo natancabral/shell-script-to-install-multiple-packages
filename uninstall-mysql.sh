52903199c6132f39513cafcbbb86e33514e77943
Natan Cabral <natancabral@gnial.com.br>
2020-10-20 23:39:23 -0300

    Create uninstall-mysql.sh

---
+++ uninstall-mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get remove --purge mysql*
+sudo apt-get purge mysql*
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get remove dbconfig-mysql
+sudo apt-get dist-upgrade
+sudo apt-get install mysql-server
