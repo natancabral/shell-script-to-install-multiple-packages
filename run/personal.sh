d7e16989b0289b36c5abbb820aacdd45d06ee881
Natan Cabral <natancabral@gnial.com.br>
2020-10-23 14:54:40 -0300

    Update personal.sh

--- run/personal.sh
+++ run/personal.sh
@@ -17,29 +17,34 @@ Cyan='\033[0;36m'         # Cyan
 # Update
 echo -e "\n ${Cyan} Updating package repositories.. ${Color_Off}"
 sudo apt -qq update 
-sudo apt install software-properties-common apt-transport-https wget
+
+# Yes Default
+# --yes | -y
+# --force-yes
+# --assume-yes - yes to all
+# $ sudo apt -y install [package]
 
 # Install FLATPAK
 # sudo apt install flatpak
 
 # Install SNAP
-sudo apt install snapd
+sudo apt -y install snapd
 
 # Faster Open Apps - Preload on RAM
-sudo apt install preload
+sudo apt -y install preload
 
 # Git
-sudo apt install git
+sudo apt -y install git
 
 # Keepass
 echo -e "\n ${Cyan} Install Keepass.. ${Color_Off}"
-sudo snap install keepassxc
+sudo snap -y install keepassxc
 
 # VLC
-sudo snap install vlc
+sudo snap -y install vlc
 
 # Gnome Boxes Virtual Machine
-sudo apt install gnome-boxes
+sudo apt -y install gnome-boxes
 # sudo flatpak install flathub org.gnome.Boxes
 # ***Note: At the time of installation, if any error occurs like the message : “Unable to lock the administration directory (/var/lib/dpkg/) is another process using it?”, then run the following commands to solve this problem.
 # $ sudo rm /var/lib/apt/lists/lock
@@ -51,7 +56,8 @@ sudo apt install gnome-boxes
 
 # MS Code
 echo -e "\n ${Cyan} Install Code Try (1).. ${Color_Off}"
-sudo snap install --classic code # or code-insiders
+sudo apt -y install software-properties-common apt-transport-https wget
+sudo snap -y install --classic code # or code-insiders
 # import the Microsoft GPG key using the following
 echo -e "\n ${Cyan} Install Code Try (2).. ${Color_Off}"
 echo -e "\n ${Green} Key.. ${Color_Off}"
@@ -60,7 +66,7 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 echo -e "\n ${Green} Download Code.. ${Color_Off}"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
-sudo apt install code
+sudo apt -y install code
 # Or
 # Download .deb
 # https://code.visualstudio.com/Download
