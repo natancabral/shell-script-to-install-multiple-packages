0660bb8f808089be4b54ba9a363dad29519ee1bb
Natan Cabral <natancabral@gnial.com.br>
2020-10-23 16:32:06 -0300

    Update lamp.sh

--- run/lamp.sh
+++ run/lamp.sh
@@ -32,7 +32,10 @@ PASS_PHPMYADMIN_ROOT="${PASS_MYSQL_ROOT}" # Your MySQL root pass
 # Check if running as root  
 if [ "$(id -u)" != "0" ]; then  
   echo -e "\n ${Yellow} This script must be run as root ${Color_Off}" 
-  echo -e " ${Yellow} Try change to user root: sudo su -- ${Color_Off}" 1>&2 
+  echo -e " ${Yellow} Try change to user root: sudo su -- or sudo su - userroot ${Color_Off}" 
+  echo -e " ${Blue} sudo su -- or ${Color_Off}" 
+  echo -e " ${Blue} sudo su - [root username] ${Color_Off}" 
+  echo -e "" 1>&2 
   exit 1  
 fi
 
