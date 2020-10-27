2b7e31b074298582d305ba776ec6ab7d54812c5d
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 19:27:07 -0300

    Update talk-to-me.sh

--- run/talk-to-me.sh
+++ run/talk-to-me.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exec
-# wget --no-cache -O - https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/personal.sh | bash
+# wget --no-cache -O - https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/talk-to-me.sh.sh | bash
 
 echo "Please talk to me ..."
 while :
