f5bfd5bb7a4ba3b3e71d6ca5707db1d033b6a5da
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 20:17:57 -0300

    Update talk-to-me.sh

--- run/talk-to-me.sh
+++ run/talk-to-me.sh
@@ -6,17 +6,28 @@
 # curl https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/talk-to-me.sh > talk.sh
 # bash talk.sh
 
+# Color Reset
+Color_Off='\033[0m'       # Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
 while : 
 do
 echo "Install "
-echo "1) MySql "
-echo "2) Php7 "
-echo "3) Apache2 "
-echo "4) PhpMyAdmin "
-echo "11) Uau! "
-echo "y) Yes "
-echo "n) No "
-echo "x) Exit "
+echo "${Yellow}1${Color_Off}) ${Cyan}MySql${Color_Off} "
+echo "${Yellow}2${Color_Off}) ${Cyan}Php7${Color_Off} "
+echo "${Yellow}3${Color_Off}) ${Cyan}Apache2${Color_Off} "
+echo "${Yellow}4${Color_Off}) ${Cyan}PhpMyAdmin${Color_Off} "
+echo "${Yellow}11${Color_Off}) ${Cyan}Uau!${Color_Off} "
+echo "${Yellow}y${Color_Off}) ${Cyan}Yes${Color_Off} "
+echo "${Yellow}n${Color_Off}) ${Cyan}No${Color_Off} "
+echo "${Yellow}x${Color_Off}) ${Cyan}Exit${Color_Off} "
 
   read -p "Select options" -n 2 choice
   echo 
