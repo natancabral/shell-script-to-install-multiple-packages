b761a6576c8aaa8a078a3bb18e8f6027ad9dc0b7
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 20:16:04 -0300

    Update talk-to-me.sh

--- run/talk-to-me.sh
+++ run/talk-to-me.sh
@@ -6,31 +6,39 @@
 # curl https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/talk-to-me.sh > talk.sh
 # bash talk.sh
 
-# read -p "Continue (y/n)?" -n 1 choice
-# case $choice in 
-
+while : 
+do
 echo "Install "
 echo "1) MySql "
 echo "2) Php7 "
 echo "3) Apache2 "
 echo "4) PhpMyAdmin "
+echo "11) Uau! "
 echo "y) Yes "
 echo "n) No "
 echo "x) Exit "
-while : 
-do
-  read INPUT_STRING
-  case $INPUT_STRING in
+
+  read -p "Select options" -n 2 choice
+  echo 
+  case $choice in 
     1) echo "Installing MySql";;
     2) echo "Installing Php7";;
     3) echo "Installing Apache2";;
     4) echo "Installing PhpMyAdmin";;
+    11) echo "Installing Uau";;
     n|N) echo "no";;
-    x|X) echo "Bye Bye" break;;
+    x|X) echo "Bye Bye" 
+    break
+    ;;
     *) echo "invalid";;
   esac
 done
 echo "Go home"
+
+echo # break line
+echo # break line
+
+echo "New while and case... " 
 
 echo # break line
 echo # break line
