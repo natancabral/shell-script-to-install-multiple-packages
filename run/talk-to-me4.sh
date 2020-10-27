0ae0be919480a72e5671924d9682b5c41825e7f5
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 20:04:43 -0300

    Update talk-to-me4.sh

--- run/talk-to-me4.sh
+++ run/talk-to-me4.sh
@@ -6,29 +6,6 @@
 # curl https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/talk-to-me.sh > talk.sh
 # bash talk.sh
 
-echo "Please talk to me ..."
-while :
-do
-  read INPUT_STRING
-  case $INPUT_STRING in
-	hello)
-		echo "Hello yourself!"
-		;;
-	bye)
-		echo "See you again!"
-		break
-		;;
-	*)
-		echo "Sorry, I don't understand"
-		;;
-  esac
-done
-echo 
-echo "That's all folks!"
-
-echo # break line
-echo # break line
-
 echo "Install "
 echo "\n 1) MySql "
 echo "\n 2) Php7 "
@@ -53,3 +30,26 @@ do
   esac
 do
 echo "Go home"
+
+echo # break line
+echo # break line
+
+echo "Please talk to me ..."
+while :
+do
+  read INPUT_STRING
+  case $INPUT_STRING in
+	hello)
+		echo "Hello yourself!"
+		;;
+	bye)
+		echo "See you again!"
+		break
+		;;
+	*)
+		echo "Sorry, I don't understand"
+		;;
+  esac
+done
+echo 
+echo "That's all folks!"
