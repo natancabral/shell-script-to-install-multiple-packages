b529e4ac66a22e9e75a4726e197120a34af90ba4
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 20:06:26 -0300

    Update talk-to-me5.sh

--- run/talk-to-me5.sh
+++ run/talk-to-me5.sh
@@ -6,19 +6,20 @@
 # curl https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config-lamp/main/run/talk-to-me.sh > talk.sh
 # bash talk.sh
 
-echo "Install "
-echo "\n 1) MySql "
-echo "\n 2) Php7 "
-echo "\n 3) Apache2 "
-echo "\n 4) PhpMyAdmin "
-echo "\n y) Yes "
-echo "\n n) No "
-echo "\n X) Exit "
-while :;
-do
-  read INPUT_STRING
 # read -p "Continue (y/n)?" -n 1 choice
 # case $choice in 
+
+echo "Install "
+echo "1) MySql "
+echo "2) Php7 "
+echo "3) Apache2 "
+echo "4) PhpMyAdmin "
+echo "y) Yes "
+echo "n) No "
+echo "x) Exit "
+while : 
+do
+  read INPUT_STRING
   case $INPUT_STRING in
     1) echo "Installing MySql";;
     2) echo "Installing Php7";;
