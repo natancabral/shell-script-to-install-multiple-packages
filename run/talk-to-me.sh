a6b416e2558a5c55e5d4efda6a1263a648c30748
Natan Cabral <natancabral@gnial.com.br>
2020-10-27 19:51:39 -0300

    Update talk-to-me.sh

--- run/talk-to-me.sh
+++ run/talk-to-me.sh
@@ -22,3 +22,25 @@ do
 done
 echo 
 echo "That's all folks!"
+
+while :
+do
+echo "Install "
+echo "\n 1) MySql "
+echo "\n 2) Php7 "
+echo "\n 3) Apache2 "
+echo "\n 4) PhpMyAdmin "
+echo "\n y) Yes "
+echo "\n n) No "
+echo "\n X) Exit "
+
+read -p "Continue (y/n)?" -n 1 choice
+case "$choice" in 
+  1 ) echo "Installing MySql";;
+  2 ) echo "Installing Php7";;
+  3 ) echo "Installing Apache2";;
+  4 ) echo "Installing PhpMyAdmin";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
+do
