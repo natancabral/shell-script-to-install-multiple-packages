7d5e9a25d71f5a12cbf96b79f2df8ac1d78e17ea
Natan Cabral <natancabral@gnial.com.br>
2020-11-08 22:43:06 -0300

    Create java.sh

---
+++ run/java.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install
+# wget --no-cache -O - https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config/main/run/java.sh | bash
+
+# Color Reset
+Color_Off='\033[0m'       # Reset
+
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+
+# Update
+echo -e "\n ${Cyan} Updating package repositories.. ${Color_Off}"
+sudo apt -qq update 
+
+# Install SNAP
+echo -e "\n ${Cyan} Snap Snapd.. ${Color_Off}"
+sudo apt -y install snapd
+sudo apt -y install snap
+
+# NetBeans Stable
+echo -e "\n ${Cyan} Java NetBeans.. ${Color_Off}"
+sudo snap install netbeans --classic
