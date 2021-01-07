6a3faea62003ed8e2645a0c76409e50b10c7b79b
Natan Cabral <natancabral@gnial.com.br>
2021-01-07 18:22:13 -0300

    Update uninstall-lamp.sh

--- run/uninstall-lamp.sh
+++ run/uninstall-lamp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install
-# wget --no-cache -O - https://raw.githubusercontent.com/natancabral/ubuntu-bash-script-config/main/run/uninstall-lamp.sh | bash
+# wget --no-cache -O - https://raw.githubusercontent.com/natancabral/shell-script-to-install-multiple-packages/main/run/uninstall-lamp.sh | bash
 
 # Color Reset
 Color_Off='\033[0m'       # Reset
@@ -15,32 +15,32 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 
 # Uninstall Mysql
-echo -e "\n ${Yellow} Uninstall MySql.. ${Color_Off}"
+echo -e "\n${Red} * Uninstall MySql.. ${Color_Off}"
 sudo apt-get remove --purge mysql*
 sudo apt-get purge mysql*
 # sudo apt-get autoremove
 # sudo apt-get remove dbconfig-mysql
 
 # remove phpmyadmin
-echo -e "\n ${Yellow} Uninstall PhpMyAdmin.. ${Color_Off}"
+echo -e "\n${Red} * Uninstall PhpMyAdmin.. ${Color_Off}"
 sudo apt-get purge phpmyadmin # or
 sudo apt-get remove phpmyadmin
 # sudo apt-get autoremove
 
 # remove php
-echo -e "\n ${Yellow} Uninstall Php.. ${Color_Off}"
+echo -e "\n${Red} * Uninstall Php.. ${Color_Off}"
 sudo apt-get purge php.*
 # sudo apt-get autoremove
 
 # remove apache2
-echo -e "\n ${Yellow} Uninstall Apache 2.. ${Color_Off}"
+echo -e "\n${Red} * Uninstall Apache 2.. ${Color_Off}"
 sudo service apache2 stop
 sudo apt-get remove apache2*
 # sudo apt purge apache2 apache2-utils apache2.2-bin apache2-common
 # sudo apt-get autoremove
 
 # update
-echo -e "\n ${Yellow} Auto Clean.. ${Color_Off}"
+echo -e "\n${Red} * Auto Clean.. ${Color_Off}"
 sudo apt-get autoclean
 sudo apt-get autoremove
 sudo apt-get dist-upgrade
