0185a4094d84e131c5d760c3704a18cab81895b4
Natan Cabral <natancabral@gnial.com.br>
2021-12-29 13:14:57 -0300

    Update personal.sh

--- run/personal.sh
+++ run/personal.sh
@@ -110,7 +110,11 @@ sudo apt-get -y install curl
 
 # Flatpak - Package Manager
 echo -e "\n${Cyan} * Flatpak.. ${Color_Off}"
-sudo apt-get -y install flatpak
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt -y install flatpak
+sudo apt install gnome-software-plugin-flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Snap - Package Manager
 echo -e "\n${Cyan} * Snap / Snapd ${Color_Off}"
