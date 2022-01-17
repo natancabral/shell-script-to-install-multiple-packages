c4bc78d0edeaefd8515a25f1c7c3ee9d32634998
Natan Cabral <natancabral@gnial.com.br>
2022-01-17 12:41:58 -0300

    Update terminal-shell.sh

--- run/terminal-shell.sh
+++ run/terminal-shell.sh
@@ -31,9 +31,9 @@ echo -e "\n${Green} * Install Starship ${Color_Off}"
 # https://starship.rs/
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-echo -e "\n${Yellow} * e-val \"\$\(starship init bash\)\" ${Color_Off}"
+echo -e "\n${Yellow} * starship init bash ${Color_Off}"
 sudo nano ~/.bashrc
 # eval "$(starship init bash)"
-echo -e "\n${Yellow} * e-val \"\$\(starship init zsh\)\" ${Color_Off}"
-nano ~/.zshrc
+echo -e "\n${Yellow} * starship init zsh ${Color_Off}"
+sudo nano ~/.zshrc
 # eval "$(starship init zsh)"
